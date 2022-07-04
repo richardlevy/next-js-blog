13df20e2953f7c14dabdf2a41f44adc18e26855d
Rich <richard.levy@radtac.com>
2022-07-04 13:42:36 +0100

    Initial commit

---
+++ pages/404.js
@@ -0,0 +1,5 @@
+function fn404() {
+    return <h1>My 404 page!</h1>
+}
+
+export default fn404;
---
+++ pages/posts/first-post.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+import Script from 'next/script'
+import Layout from '../../components/layout';
+
+export default function FirstPost() {
+    return (
+        <Layout>
+            <Head>
+                <title>T : First post</title>
+            </Head>
+            { /* - script example - note that it's NOT in the Head 
+            <Script
+                src="https://connect.facebook.net/en_US/sdk.js"
+                strategy="lazyOnload"
+                onLoad={() =>
+                    console.log(`script loaded correctly, window.FB has been populated`)
+                }
+            />
+            */ }
+            <h1>First Post</h1>
+        </Layout>
+    )
+}
